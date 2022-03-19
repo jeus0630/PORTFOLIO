595b8f2eb8add2844aa955fbb84d37a9bc7da2c3
Jewoo <jeus0630@gmail.com>
2022-03-19 11:38:45 +0900

    Add : Three.js
    Dog ThreeJS Rendering

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,25 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layouts/main";
 import theme from "../lib/theme";
 import Fonts from '../components/fonts';
+import { AnimatePresence } from 'framer-motion'
+
 
 const Website = ({ Component, pageProps, router }) => {
     return(
         <ChakraProvider them={theme}>
             <Fonts></Fonts>
             <Layout router={router}>
-                <Component {...pageProps} key={router.route}></Component>
+                <AnimatePresence
+                    exitBeforeEnter
+                    initial={true}
+                    onExitComplete={() => {
+                        if (typeof window !== 'undefined') {
+                        window.scrollTo({ top: 0 })
+                        }
+                    }}
+                    >
+                    <Component {...pageProps} key={router.route}></Component>
+                    </AnimatePresence>
             </Layout>
         </ChakraProvider>
 
