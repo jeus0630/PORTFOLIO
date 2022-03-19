3a44588fbfa4e52075b2d1c5a5cf0fb2fef23921
Jewoo <jeus0630@gmail.com>
2022-03-19 10:39:02 +0900

    Add : bio.js
    bio & paragraph

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
+import { Link, Container, Box, Heading, Image, useColorModeValue, Button } from "@chakra-ui/react";
 import Section from '../components/section'
+import Paragraph from "../components/paragraph";
+import NextLink from 'next/link'
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { BioSection, BioYear } from '../components/bio'
+
 
 const Page = () => {
     return (
@@ -23,9 +28,68 @@ const Page = () => {
                 <Heading as="h3" variant="section-title">
                     Work
                 </Heading>
-                <p>Paragraph</p>
+                <Paragraph>
+                    Takuya is a freelance and a full-stack developer based in Osaka with a
+                    passion for building digital services/stuff he wants. He has a knack
+                    for all things launching products, from planning and designing all the
+                    way to solving real-life problems with code. When not online, he loves
+                    hanging out with his camera. Currently, he is living off of his own
+                    product called{' '}
+                     <NextLink href="/works/inkdrop" scroll={false}>
+                        <Link>Inkdrop</Link>
+                    </NextLink>
+                    .
+                </Paragraph>
+                <Box align="center" my={4}>
+                    <NextLink href="/works">
+                        <Button rightIcon={<ChevronRightIcon></ChevronRightIcon>} colorScheme="teal">
+                            My portfolio
+                        </Button>
+                    </NextLink>
+                </Box>
             </Section>
+             <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1984</BioYear>
+          Born in Osaka (大阪), Japan.
+        </BioSection>
+        <BioSection>
+          <BioYear>2010</BioYear>
+          Completed the Master&apos;s Program in the Graduate School of
+          Information Science at Nara Institute of Science and Technology
+          (奈良先端科学技術大学院大学情報科学研究科修士課程)
+        </BioSection>
+        <BioSection>
+          <BioYear>2010</BioYear>
+          Worked at Yahoo! Japan (ヤフー株式会社入社)
+        </BioSection>
+        <BioSection>
+          <BioYear>2012 to present</BioYear>
+          Works as a freelance
+        </BioSection>
+            </Section>
+            <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Art, Music,{' '}
+          <Link href="https://illust.odoruinu.net/" target="_blank">
+            Drawing
+          </Link>
+          , Playing Drums,{' '}
+          <Link href="https://500px.com/p/craftzdog" target="_blank">
+            Photography
+          </Link>
+          , Leica, Machine Learning
+        </Paragraph>
+      </Section>
+
         </Container>
+        
         
     )
 }
